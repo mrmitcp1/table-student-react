ad874ba92ce84ec41ea46a8823de5dcddfcc6d20
mrmitcp1 <mrmitcp55@gmail.com>
2023-07-05 16:28:52 +0700

    student

--- src/index.js
+++ src/index.js
@@ -4,11 +4,56 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const students = [
+    {
+        company: 'Alfreds Futterkiste',
+        contact: 'Maria Anders',
+        country: 'Germany'
+    },
+    {
+        company: 'Centro comercial Moctezuma',
+        contact: 'Francisco Chang',
+        country: 'Mexico'
+    },
+    {
+        company: 'Ernst Handel',
+        contact: 'Roland Mendel',
+        country: 'Austria'
+    },
+    {
+        company: 'Island Trading',
+        contact: 'Helen Bennett',
+        country: 'UK'
+    },
+    {
+        company: 'Laughing Bacchus Winecellars',
+        contact: 'Yoshi Tannamuri',
+        country: 'Canada'
+    },
+    {
+        company: 'Magazzini Alimentari Riuniti',
+        contact: 'Giovanni Rovelli',
+        country: 'Italy'
+    }
+]
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <>
+        <h1>Student List</h1>
+        <tr>
+            <td>Company</td>
+            <td>Contact</td>
+            <td>Country</td>
+        </tr>
+    {students.map(student=>(
+        <tr>
+            <td>{student.company}</td>
+            <td>{student.contact}</td>
+            <td>{student.country}</td>
+        </tr>
+        ))}
+    </>
 );
 
 // If you want to start measuring performance in your app, pass a function
